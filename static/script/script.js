8da00aa1e0702eb83eb0801618350aeb32a8bd2c
Rich-Cardo <123091712+Rich-Cardo@users.noreply.github.com>
2024-02-06 17:48:20 -0400

    Update script.js

--- static/script/script.js
+++ static/script/script.js
@@ -145,7 +145,7 @@ if (currentTemplate === 'create_factura') {
         var keyCode = event.keyCode || event.which;
         if (keyCode < 48 || keyCode > 57) {
             event.preventDefault();
-    }
+        }
     });
 
     // Agregar un evento de escucha para el evento keypress en cada input
@@ -171,13 +171,16 @@ if (currentTemplate === 'create_factura') {
         var keyCode = event.keyCode || event.which;
         if (keyCode < 48 || keyCode > 57) {
             event.preventDefault();
-    }
+        }
     });
 
     precioInput.addEventListener('keypress', function(event) {
         var keyCode = event.keyCode || event.which;
         if (keyCode < 48 || keyCode > 57) {
             event.preventDefault();
+        }
+    });
+
     // Agregar un evento de escucha para el evento keypress en cada input
     nombreInput.addEventListener('keypress', function(event) {
         // Obtener el código de la tecla presionada
@@ -204,79 +207,6 @@ if (currentTemplate === 'create_factura') {
             event.preventDefault(); 
         } 
     });
-}
-
-if (currentTemplate === 'create_factura') {
-
-    var cedulaInput = document.getElementById('txtCedula');
-    var nombreInput = document.getElementById('txtNombre');
-    var apellidoInput = document.getElementById('txtApellido');
-    var cantidadInput = document.getElementById('txtCantidad');
-    var precioInput = document.getElementById('txtPrecio');
-
-    // Funciones para validar los campos:
-
-    cedulaInput.addEventListener('keypress', function(event) {
-        var keyCode = event.keyCode || event.which;
-        if (keyCode < 48 || keyCode > 57) {
-            event.preventDefault();
-    }
-    });
-
-    // Agregar un evento de escucha para el evento keypress en cada input
-    nombreInput.addEventListener('keypress', function(event) {
-        // Obtener el código de la tecla presionada
-        var keyCode = event.keyCode || event.which;
-        
-        // Verificar si el código corresponde a una letra o espacio y cancelar el evento si no es así
-        if ((keyCode < 65 || keyCode > 90) && (keyCode < 97 || keyCode > 122) && keyCode !== 32) {
-            event.preventDefault();
-        }
-    });
-        
-
-    apellidoInput.addEventListener('keypress', function(event) {
-    var keyCode = event.keyCode || event.which;
-        if ((keyCode < 65 || keyCode > 90) && (keyCode < 97 || keyCode > 122) && keyCode !== 32) {
-            event.preventDefault();
-        }
-    });
-
-    cantidadInput.addEventListener('keypress', function(event) {
-        var keyCode = event.keyCode || event.which;
-        if (keyCode < 48 || keyCode > 57) {
-            event.preventDefault();
-    }
-    });
-
-    precioInput.addEventListener('keypress', function(event) {
-        var keyCode = event.keyCode || event.which;
-        if (keyCode < 48 || keyCode > 57) {
-            event.preventDefault();
-    }
-    });
-    });
-
-}
-
-function calcularEdad() {
-    // Obtener el valor de la fecha de nacimiento
-    var fechaNacimiento = document.getElementById("txtFecha_Nac").value;
-  
-    // Calcular la fecha actual
-    var fechaActual = new Date();
-  
-    // Calcular la diferencia en milisegundos entre la fecha actual y la fecha de nacimiento
-    var diferencia = fechaActual - new Date(fechaNacimiento);
-  
-    // Convertir la diferencia en años
-    var edad = Math.floor(diferencia / 31557600000); // aproximadamente la cantidad de milisegundos en un año
-  
-    // Asignar la edad calculada al campo "txtEdad"
-    document.getElementById("txtEdad").value = edad;
-}
-
-// // Funcion para retroceder
 }
 
 function calcularEdad() {
